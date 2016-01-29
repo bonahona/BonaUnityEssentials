a2f0583858058fb543c43fd9b8a4d440e038eccf
Björn Fyrvall <bjorn.fyrvall@softronic.se>
2016-01-29 11:00:05 +0100

    Added initial functionality. Includes a lot of handy extension classes for primitives and some custom property drawers.

---
+++ Assets/BonaUnityEssentials/Engine/ObjectExtension.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public static class ObjectExtension
+{
+    public static T To<T>(this object o)
+    {
+        return (T)o;
+    }
+
+    public static T ToOrDefault<T>(this object o)
+    {
+        try {
+            return (T)o;
+        } catch (Exception) {
+            return default(T);
+        }
+    }
+}
---
+++ Assets/Test/SceneTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTest : MonoBehaviour {
+
+    [Scene]
+    public string SceneToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
